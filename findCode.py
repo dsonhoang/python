d198a84ece2d94bdff579f16419f77021e2b957b
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-06-04 20:48:49 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -5,7 +5,7 @@ def find(driver2, link_to_open):
   driver2.get(link_to_open)
   text_value = ''
   try:
-    text_value = drive2.find_element(By.ID, 'kode').text
+    text_value = driver2.find_element(By.ID, 'kode').text
   except:
     pass
   return text_value
