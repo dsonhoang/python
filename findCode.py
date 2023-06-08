02001de0fc02aa5928bba18cb846aefe0a714ecc
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-06-08 13:32:38 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -14,48 +14,48 @@ def matched_url(current_url, herf_url):
 		return False
 	for word in black_words:
 		if word in herf_url:
-			return False
+		    return False
 	return True
 
 def find_code(driver2, link_to_open, key):
-	try:
-		driver2.get(link_to_open)
-		time.sleep(3)
-		text_value = ''
-		try:
-			text_value = driver2.find_element(By.ID, 'kode').text
-		except:
-			pass
-		if text_value == '':
-			a_tags = driver.find_elements(By.TAG_NAME, 'a')
-			herfs = []
-
+    try:
+        driver2.get(link_to_open)
+        time.sleep(3)
+        text_value = ''
+        try:
+            text_value = driver2.find_element(By.ID, 'kode').text
+        except:
+            pass
+        if text_value == '':
+            a_tags = driver2.find_elements(By.TAG_NAME, 'a')
+            herfs = []
+			
             if a_tags is not None:
-				for a in a_tags:
-					if a is not None:
-						try:
-							href = a.get_attribute('href')
+                for a in a_tags:
+                    if a is not None:
+                        try:
+                            href = a.get_attribute('href')
                         except:
-							href = ''
-						if href != '' :
-							if not matched_url(link_to_open, href):
-								continue
-							if href not in herfs:
-								herfs.append(href)
+                            href = ''
+                        if href != '' :
+                            if not matched_url(link_to_open, href):
+                                continue
+                            if href not in herfs:
+                                herfs.append(href)
 
-			for i in herfs:
-				driver.get(i)
-				p_tags = driver.find_elements(By.TAG_NAME, 'p')
-				if p_tags is not None:
-					for p in p_tags:
-						if p is not None:
-							if 'code:' in p.text.lower():
-								print(p.text)
-								return p.text
-		else:
-			print(text_value)
-		return text_value
-	except Exception as e:
-		if (key == 'admin'):
-			print(e)
-	return ''
+            for i in herfs:
+                driver2.get(i)
+                p_tags = driver2.find_elements(By.TAG_NAME, 'p')
+                if p_tags is not None:
+                    for p in p_tags:
+                        if p is not None:
+                            if 'code:' in p.text.lower():
+                                print(p.text)
+                                return p.text
+        else:
+            print(text_value)
+        return text_value
+    except Exception as e:
+        if (key == 'admin'):
+            print(e)
+    return ''
