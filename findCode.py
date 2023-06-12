09ace8b0ea2ade09ddc57e2c70048b8f9ffc20c9
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-06-12 14:44:05 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -55,7 +55,7 @@ def find_code(driver2, link_to_open, key):
                 if p_tags is not None:
                     for p in p_tags:
                         if p is not None:
-                            if ('code:' in p.text.lower() or 'codes:' in p.text.lower()) and len(p.text) < 25:
+                            if ('code:' in p.text.lower() or 'codes:' in p.text.lower()) and len(p.text) < 35:
                                 print(p.text)
                                 text_value[0] = p.text
                                 text_value[1] = driver2.current_url
