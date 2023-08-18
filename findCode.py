d8e8222f5af25074c2f5f39a446aaafc862c0e25
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-08-18 13:57:48 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -27,6 +27,16 @@ def find_code(driver2, sorted_url, key):
                                 text_value[0] = p.text.split(':')[1].strip()
                                 text_value[1] = driver2.current_url
                                 return text_value
+                                
+                h2_tags = driver2.find_elements(By.TAG_NAME, 'h2')
+                if h2_tags is not None:
+                    for p in h2_tags:
+                        if p is not None:
+                            if ('code :' in p.text.lower() or 'code:' in p.text.lower() or 'codes:' in p.text.lower() or 'hint cd:' in p.text.lower()) and len(p.text) < 35 and len(p.text) > 6:
+                                print(p.text)
+                                text_value[0] = p.text.split(':')[1].strip()
+                                text_value[1] = driver2.current_url
+                                return text_value
         return text_value
     except Exception as e:
         print(e)
