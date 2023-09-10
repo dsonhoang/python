079e1d66ba97514e85de18eab5734ccfd183c7e3
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-09-10 19:36:26 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -46,7 +46,7 @@ async def find_code(page, sorted_url, key):
             if p_tags:
                 for p_element in p_tags:
                     text_lower_ = await page.evaluate('(element) => element.textContent', p_element)
-                    text_lower = text_lower.lower()
+                    text_lower = text_lower_.lower()
                     if any(keyword in text_lower for keyword in ['code :', 'code:', 'codes:', 'codes :', 'hint cd:']) and 6 < len(text_lower) < 35:
                         text_value[0] = text_lower_.split(':')[1].strip()
                         text_value[1] = page.url
