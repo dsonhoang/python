6da69606f7feb6f0d33eca069c41799765633f16
dsonhoang <73021244+dsonhoang@users.noreply.github.com>
2023-09-10 13:27:02 +0700

    Update findCode.py

--- findCode.py
+++ findCode.py
@@ -45,10 +45,10 @@ async def find_code(page, sorted_url, key):
             p_tags = await page.querySelectorAll('p, li, h1, h2, h3, strong')
             if p_tags:
                 for p_element in p_tags:
-                    text_lower = await page.evaluate('(element) => element.textContent', p_element)
+                    text_lower_ = await page.evaluate('(element) => element.textContent', p_element)
                     text_lower = text_lower.lower()
                     if any(keyword in text_lower for keyword in ['code :', 'code:', 'codes:', 'codes :', 'hint cd:']) and 6 < len(text_lower) < 35:
-                        text_value[0] = text_lower.split(':')[1].strip()
+                        text_value[0] = text_lower_.split(':')[1].strip()
                         text_value[1] = page.url
                         return text_value
 
